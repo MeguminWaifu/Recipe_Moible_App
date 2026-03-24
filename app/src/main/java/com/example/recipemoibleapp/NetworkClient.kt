da5b1a5e5f963a19bc24ac780db090eb16ccca2d
MeguminWaifu <raphaelcorrea63@gmail.com>
2026-03-24 19:01:27 +0800

    Added a lot not gonna lie, added recipe cards for homescreen, made a SignUpScreen page, added function to add recipe button, made CreateRecipeScreen of which has its own modals and an upload of image from phone gallery

---
+++ app/src/main/java/com/example/recipemoibleapp/NetworkClient.kt
@@ -0,0 +1,14 @@
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+val client = HttpClient(Android) {
+    install(ContentNegotiation) {
+        json(Json {
+            ignoreUnknownKeys = true // Prevents crashing if the API sends extra data
+            prettyPrint = true
+        })
+    }
+}
